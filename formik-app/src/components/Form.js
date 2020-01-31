72a195a574fdb0a561bedbf1dd2aa3c038590a09
Hester Knight <hesterknight@hesters-mbp.mynet>
2020-01-31 15:57:51 +0000

    importing

--- formik-app/src/components/Form.js
+++ formik-app/src/components/Form.js
@@ -1,5 +1,7 @@
 import React from 'react';
-
+import axios from 'axios';
+import * as Yup from 'yup';
+import { Form, Formik, Field, ErrorMessage } from 'formik';
 
 export default function Form () {
 
